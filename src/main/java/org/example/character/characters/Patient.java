7a882110f80b91d713e942b74ed234acf51b5516
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-05 10:25:16 +0100

    feat: I create Patient class, using herence of Character class, with private attributes, creating constructor and getters and setters to access it at exterior clases. I make class as abstract because i don't want to make instances

--- src/main/java/org/example/character/characters/Patient.java
+++ src/main/java/org/example/character/characters/Patient.java
@@ -1,4 +1,66 @@
 package org.example.character.characters;
 
-public class Patient {
+import org.example.character.Character;
+
+import java.util.Date;
+
+
+public abstract class Patient extends Character {
+
+    private String diagnosis;
+    private String treatment;
+    private String medicAssigned;
+
+    private int numberRoom;
+
+    private boolean isHospitalized;
+
+    public Patient(String name, String dni, String address, String email, int phone, char gender, Date birthdate, String diagnosis, String treatment, String medicAssigned, int numberRoom, boolean isHospitalized) {
+        super(name, dni, address, email, phone, gender, birthdate);
+        this.diagnosis = diagnosis;
+        this.treatment = treatment;
+        this.medicAssigned = medicAssigned;
+        this.numberRoom = numberRoom;
+        this.isHospitalized = isHospitalized;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getMedicAssigned() {
+        return medicAssigned;
+    }
+
+    public void setMedicAssigned(String medicAssigned) {
+        this.medicAssigned = medicAssigned;
+    }
+
+    public int getNumberRoom() {
+        return numberRoom;
+    }
+
+    public void setNumberRoom(int numberRoom) {
+        this.numberRoom = numberRoom;
+    }
+
+    public boolean isHospitalized() {
+        return isHospitalized;
+    }
+
+    public void setHospitalized(boolean hospitalized) {
+        isHospitalized = hospitalized;
+    }
 }
