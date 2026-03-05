f7cdb80e742e78fd0032d52102147adfbc6d19d4
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-05 13:00:21 +0100

    feat: I create Medic class, using herence of Character class, with private attributes, creating constructor and getters and setters to access it at exterior clases. I make class as abstract because i don't want to make instances. Also i implements interfaces to this class(common methods) and a print method

--- src/main/java/org/example/character/characters/Medic.java
+++ src/main/java/org/example/character/characters/Medic.java
@@ -1,4 +1,105 @@
 package org.example.character.characters;
 
-public class Medic {
+import org.example.character.Character;
+import org.example.character.characters.actions.medics.AllMedics;
+
+import java.time.LocalDate;
+
+public abstract class Medic extends Character implements AllMedics {
+
+    private String nameHospital;
+    private String schedule;
+    private String shift;
+
+    private int numberColegial;
+    private int salary;
+    private int hoursWorkWeek;
+
+    private LocalDate dateAdmission;
+
+    public Medic(String name, String dni, String address, String email, String phone, char gender, LocalDate birthdate, String nameHospital, String schedule, String shift, int numberColegial, int salary, int hoursWorkWeek, LocalDate dateAdmission) {
+        super(name, dni, address, email, phone, gender, birthdate);
+        this.nameHospital = nameHospital;
+        this.schedule = schedule;
+        this.shift = shift;
+        this.numberColegial = numberColegial;
+        this.salary = salary;
+        this.hoursWorkWeek = hoursWorkWeek;
+        this.dateAdmission = dateAdmission;
+    }
+
+    @Override
+    public String displayInfoCharacter(){
+        return super.displayInfoCharacter() +
+                "\nNombre del hospital: " + nameHospital +
+                "\nHorario: " + schedule +
+                "\nTurno de trabajo: " + shift +
+                "\nSalario: " + salary +
+                "\nHoras de trabajo a la semana: " + hoursWorkWeek;
+    }
+
+    @Override
+    public void hoursWorkWeek(){
+        System.out.println("El médico/estudiante trabaja " +  hoursWorkWeek + " horas a la semana");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("El médico/estudiante está comiendo");
+    }
+    public String getNameHospital() {
+        return nameHospital;
+    }
+
+    public void setNameHospital(String nameHospital) {
+        this.nameHospital = nameHospital;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public int getNumberColegial() {
+        return numberColegial;
+    }
+
+    public void setNumberColegial(int numberColegial) {
+        this.numberColegial = numberColegial;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getDateAdmission() {
+        return dateAdmission;
+    }
+
+    public void setDateAdmission(LocalDate dateAdmission) {
+        this.dateAdmission = dateAdmission;
+    }
+
+    public int getHoursWorkWeek() {
+        return hoursWorkWeek;
+    }
+
+    public void setHoursWorkWeek(int hoursWorkWeek) {
+        this.hoursWorkWeek = hoursWorkWeek;
+    }
 }
---
+++ src/main/java/org/example/character/characters/actions/medics/AllMedics.java
@@ -0,0 +1,8 @@
+package org.example.character.characters.actions.medics;
+
+public interface AllMedics {
+
+    public void hoursWorkWeek();
+    public void eat();
+
+}
