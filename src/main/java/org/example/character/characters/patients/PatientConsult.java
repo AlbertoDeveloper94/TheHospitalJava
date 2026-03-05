069656b4426fd78cc3421b5a92343c8cafb8425a
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-05 11:30:34 +0100

    feat: I create PatientConsult class, using herence of Patient class, with private attributes, creating constructor and getters and setters to access it at exterior clases.

--- src/main/java/org/example/character/characters/patients/PatientConsult.java
+++ src/main/java/org/example/character/characters/patients/PatientConsult.java
@@ -1,4 +1,43 @@
 package org.example.character.characters.patients;
 
-public class PatientConsult {
+import org.example.character.characters.Patient;
+
+import java.time.LocalDate;
+
+public class PatientConsult extends Patient {
+
+    private int numberWaitingRoom;
+    private LocalDate appointmentDate;
+    private String appoinmentStatus;
+
+    public PatientConsult(String name, String dni, String address, String email, String phone, char gender, LocalDate birthdate, String diagnosis, String treatment, String medicAssigned, int numberRoom, boolean isHospitalized, int numberWaitingRoom, LocalDate appointmentDate, String appoinmentStatus) {
+        super(name, dni, address, email, phone, gender, birthdate, diagnosis, treatment, medicAssigned, numberRoom, isHospitalized);
+        this.numberWaitingRoom = numberWaitingRoom;
+        this.appointmentDate = appointmentDate;
+        this.appoinmentStatus = appoinmentStatus;
+    }
+
+    public int getNumberWaitingRoom() {
+        return numberWaitingRoom;
+    }
+
+    public void setNumberWaitingRoom(int numberWaitingRoom) {
+        this.numberWaitingRoom = numberWaitingRoom;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppoinmentStatus() {
+        return appoinmentStatus;
+    }
+
+    public void setAppoinmentStatus(String appoinmentStatus) {
+        this.appoinmentStatus = appoinmentStatus;
+    }
 }
