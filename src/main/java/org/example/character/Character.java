b0502ad73ee0d20630d2da2a523c1b309a71d62e
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-05 10:14:29 +0100

    feat: I create Character class with private attributes, creating constructor and getters and setters to access it at exterior clases

--- src/main/java/org/example/character/Character.java
+++ src/main/java/org/example/character/Character.java
@@ -1,4 +1,81 @@
 package org.example.character;
 
-public class Character {
+import java.util.Date;
+
+public abstract class Character {
+
+    private String name;
+    private String dni;
+    private String address;
+    private String email;
+
+    private int phone;
+    private char gender;
+    private Date birthdate;
+
+    public Character(String name, String dni, String address, String email, int phone, char gender, Date birthdate) {
+        this.name = name;
+        this.dni = dni;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
 }
