8b0c831d114734756b47ae0d164a356736685365
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-05 16:32:01 +0100

    feat: I create Speciality class, using herence of Medic class, with private attributes, creating constructor and getters and setters to access it at exterior clases. Also i implements interfaces to this class(speciality methods)

---
+++ src/main/java/org/example/character/characters/actions/medics/Specialists.java
@@ -0,0 +1,8 @@
+package org.example.character.characters.actions.medics;
+
+public interface Specialists {
+
+    public void dateStartHolidays();
+    public void speciality();
+
+}
--- src/main/java/org/example/character/characters/medics/Specialist.java
+++ src/main/java/org/example/character/characters/medics/Specialist.java
@@ -1,4 +1,61 @@
 package org.example.character.characters.medics;
 
-public class Specialist {
+import org.example.character.characters.Medic;
+import org.example.character.characters.actions.medics.Specialists;
+
+import java.time.LocalDate;
+
+public class Specialist extends Medic implements Specialists {
+
+    private String speciality;
+    private int experienceYears;
+    private LocalDate dateStartHolidays;
+
+    public Specialist(String name, String dni, String address, String email, String phone, char gender, LocalDate birthdate, String nameHospital, String schedule, String shift, int numberColegial, int salary, int hoursWorkWeek, LocalDate dateAdmission, LocalDate dateStartHolidays, int experienceYears, String speciality) {
+        super(name, dni, address, email, phone, gender, birthdate, nameHospital, schedule, shift, numberColegial, salary, hoursWorkWeek, dateAdmission);
+        this.dateStartHolidays = dateStartHolidays;
+        this.experienceYears = experienceYears;
+        this.speciality = speciality;
+    }
+
+    @Override
+    public String displayInfoCharacter() {
+        return super.displayInfoCharacter() +
+                "\nEspecialidad: " + speciality +
+                "\nAños de experiencia:  : " + experienceYears +
+                "\nFecha que inicia las vacaciones: " + dateStartHolidays;
+    }
+
+    @Override
+    public void dateStartHolidays(){
+        System.out.println("El médico " + getName() + " empieza las vacaciones a partir del " + dateStartHolidays);
+    };
+    @Override
+    public void speciality(){
+        System.out.println("El médico " + getName() + " se especializa en " + speciality);
+    };
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+
+    public LocalDate getDateStartHolidays() {
+        return dateStartHolidays;
+    }
+
+    public void setDateStartHolidays(LocalDate dateStartHolidays) {
+        this.dateStartHolidays = dateStartHolidays;
+    }
 }
